e66f6f3dc59e4f1a7231278da311c239de78589f
digitai <jea.data@gmail.com>
2021-08-19 18:17:11 -0500

    feat: added hook

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from '../Product/Product';
+import './App.css';
+
+function App() {
+  return (
+    <div>App es :
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Product.css';
+
+const Product = () => {
+  return(
+    <div className="wrapper">
+      <div>Shopping Cart: 0 total items.</div>
+      <div>Total: 0</div>
+      <div className="product">
+        <span role="img" aria-label="ice cream">🍦</span>
+      </div>
+      <button>Add</button>
+      <button>Remove</button>
+    </div>
+  );
+}
+
+export default Product;
